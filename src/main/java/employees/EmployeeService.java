2895d1fa60fd68afc6bd0dcb1a108738bc76cbb6
vicziani <viczian.istvan@gmail.com>
2020-11-15 19:51:07 +0100

    Constructor injection

--- src/main/java/employees/EmployeeService.java
+++ src/main/java/employees/EmployeeService.java
@@ -1,5 +1,6 @@
 package employees;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,13 +9,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
 
-    private EmployeeRepository employeeRepository;
-
-    public EmployeeService(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
+    private final EmployeeRepository employeeRepository;
 
     public List<EmployeeDto> listEmployees() {
         return employeeRepository
